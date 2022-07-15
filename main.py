0bd958ec1a36cba99e30f1a9701a13e6b2e4b96e
naveenkumar <naveenbti002@gmail.com>
2022-07-14 22:17:32 -0400

    program done

---
+++ main.py
@@ -0,0 +1,46 @@
+import openpyxl
+from openpyxl import Workbook
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+def searchAndSave(r):
+
+    url = 'https://www.bing.com/packagetrackingv2?packNum='+trackingNo+'&carrier=UPS&FORM=PCKTR1'
+    url1 = 'https://httpbin.org/headers'
+
+    response = requests.get(url=url)
+
+    parse = BeautifulSoup(response.content,'html.parser')
+    status = parse.find('div', {"class":"b_focusTextSmall"})
+    print(status.text)
+    print(sheet.max_row)
+    row = "B"+str(r)
+    sheet[row].value = status.text.split(":")[0]
+
+#Variables-------
+trackingNoRow = "A"
+trackingNoRowStart = 2
+
+statusRow = "B"
+workbookName = "trackingUps.xlsx"
+sheetName = 'Sheet1'
+
+
+
+workbook = openpyxl.load_workbook(workbookName)
+sheet = workbook[sheetName]
+
+for row in range(trackingNoRowStart,sheet.max_row+1):
+
+    put = trackingNoRow+str(row)
+    print(put)
+    if sheet[put].value is not None:
+        trackingNo = sheet[put].value
+        searchAndSave(row)
+
+
+workbook.save("trackingUps.xlsx")
+#print(response.content)
+
+
